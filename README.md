6177cb52b16af6a1685f3add542c16b9d1bb9eae
Lukas Unbesiegt <blackhelios.web@gmail.com>
2020-04-27 04:59:13 +0630

    Socketio implementation Apr27

--- README.md
+++ README.md
@@ -6,3 +6,4 @@
 - reusable components for form handling with formik , file uploads with cropper , selectors , WYSIWYG with react-draft-wysiwyg and quill.js
 - connected react router for redux binding routing
 - default theme with argon design system
+- websocket with socket.io
--- src/store.js
+++ src/store.js
@@ -1,29 +1,40 @@
 import { createStore, applyMiddleware } from "redux";
 import { reducers } from "./reducers/index";
 import Thunk from "redux-thunk";
-
+import io from "socket.io-client";
 import { composeWithDevTools } from "redux-devtools-extension";
 import { createBrowserHistory } from "history";
 import { routerMiddleware } from "connected-react-router";
+import {
+  socketIOEmitterMiddleware,
+  socketIOSubscriberMiddleware,
+} from "./middlewares/socket";
 import { responsiveStoreEnhancer } from "redux-responsive";
-
+import { endpoint, prodEndpoint } from "./config";
+const URL = process.env.NODE_ENV === "development" ? endpoint : prodEndpoint;
 const history = createBrowserHistory();
+const socket = io.connect(URL);
 
-export const configureStore = preloadedState => {
-	const middlewares = [Thunk, routerMiddleware(history)];
-	const middlewareEnhancer = applyMiddleware(...middlewares);
-	const storeEnhancers = [middlewareEnhancer, responsiveStoreEnhancer];
+export const configureStore = (preloadedState) => {
+  const middlewares = [
+    Thunk,
+    routerMiddleware(history),
+    socketIOSubscriberMiddleware(socket),
+    socketIOEmitterMiddleware(socket),
+  ];
+  const middlewareEnhancer = applyMiddleware(...middlewares);
+  const storeEnhancers = [middlewareEnhancer, responsiveStoreEnhancer];
 
-	const composedEnhancer = composeWithDevTools(...storeEnhancers);
+  const composedEnhancer = composeWithDevTools(...storeEnhancers);
 
-	const store = createStore(
-		reducers(history),
-		preloadedState,
-		composedEnhancer
-	);
+  const store = createStore(
+    reducers(history),
+    preloadedState,
+    composedEnhancer
+  );
 
-	return {
-		store: store,
-		history: history
-	};
+  return {
+    store: store,
+    history: history,
+  };
 };
